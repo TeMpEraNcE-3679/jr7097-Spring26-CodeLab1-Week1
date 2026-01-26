62be7b9b4921620b20d2e549fb2ea7b60a3c4cc7
TeMpEraNcE-3679 <jiayuruan315@gmail.com>
2026-01-26 15:11:15 -0500

    IN CLASS CHANGE FOR WASD FILE

--- CodeLab-S25-Week1/Assets/Scripts/WASDController.cs
+++ CodeLab-S25-Week1/Assets/Scripts/WASDController.cs
@@ -3,17 +3,17 @@ using UnityEngine.InputSystem;
     
 public class WASDController : MonoBehaviour
 { 
-    Rigidbody rb;
+    Rigidbody rb; //rigidBody for the gameObject this component is attached to
+    public float moveForce = 10f; //the force we add to the GameObject
 
-    public float moveForce = 10f;
-
-    public Key keyUp = Key.W;
-
-    Keyboard keyboard = Keyboard.current;
+    public Key keyUp = Key.W; //keyUp for the new input system
+    public Key keyDown = Key.S;
+    Keyboard keyboard = Keyboard.current; //get the keyboard input for this device;
+    
     
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>(); //search for a component of this type on this GameObject
     }
 
     void FixedUpdate()
@@ -21,13 +21,17 @@ public class WASDController : MonoBehaviour
 
         if (keyboard[keyUp].isPressed)
         {
-            rb.AddForce(Vector3.up * moveForce, ForceMode.Acceleration);
+            rb.AddForce(Vector3.up * moveForce); //give the object an upward force
+        }
+
+        if (keyboard[keyDown].isPressed)
+        {
+            rb.AddForce(Vector3.down * moveForce);
         }
         
-        
-        // if(Input.GetKey(KeyCode.S))
-        // {
-        //     rb.AddForce(Vector3.down * moveForce, ForceMode.Acceleration);
-        // }
+         //if(Input.GetKey(KeyCode.S))
+         //{
+         //    rb.AddForce(Vector3.down * moveForce); //give the object a downward force
+         //}
     }
 }
